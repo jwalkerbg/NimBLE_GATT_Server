e26f9935b886fb1446f56c8f3a5e7537ec839e80
imc <i_cenov@botevgrad.com>
2026-07-12 17:26:04 +0300

    Added  clearing of s_ble.connected when the connection is not established successfully.

--- main/src/gap.c
+++ main/src/gap.c
@@ -188,6 +188,7 @@ static int gap_event_handler(struct ble_gap_event *event, void *arg) {
         }
         /* Connection failed, restart advertising */
         else {
+            s_ble.connected = false;
             start_advertising();
         }
         return rc;
