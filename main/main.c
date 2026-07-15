1c78507fe656d997977532605914854a02b2eb89
imc <i_cenov@botevgrad.com>
2026-07-15 14:05:39 +0300

    Added response thread.

--- main/main.c
+++ main/main.c
@@ -9,6 +9,10 @@
 
 static uint8_t buf[CONFIG_NIMBLE_BUFFER_SIZE] = {0};
 
+#define MS_RESPONSE BIT0
+#define MS_EXIT     BIT1
+static EventGroupHandle_t ms_event_group = NULL;
+
 esp_err_t command_cb (struct os_mbuf *param)
 {
     int len = OS_MBUF_PKTLEN(param);
@@ -19,9 +23,35 @@ esp_err_t command_cb (struct os_mbuf *param)
     ESP_LOGI(TAG, "WRITE received, length=%d", len);
     ESP_LOG_BUFFER_HEX(TAG, buf, len);
 
+    xEventGroupSetBits(ms_event_group, MS_RESPONSE);
+
     return ESP_OK;
 }
 
+static TaskHandle_t ms_response_task_handle = NULL;
+static void ms_response_task(void* pvParameters)
+{
+    ESP_LOGI(TAG,"ms_response_task entered");
+
+    while (true) {
+        EventBits_t bits = xEventGroupWaitBits(ms_event_group, MS_RESPONSE | MS_EXIT, pdTRUE, pdFALSE, portMAX_DELAY);
+        if (bits & MS_RESPONSE) {
+            static uint8_t buff[10] = {0};
+            vTaskDelay(pdMS_TO_TICKS(1 * 1000));
+            buff[0]++;
+            ble_indicate_response(buff,1);
+            ESP_LOGI(TAG, "Response sent");
+        }
+        if (bits & MS_EXIT) {
+            break;
+        }
+    }
+
+    vEventGroupDelete(ms_event_group);
+    ms_event_group = NULL;
+    ESP_LOGI(TAG,"ms_response_task exited");
+    vTaskDelete(NULL);
+}
 void app_main(void)
 {
     esp_err_t ret;
@@ -44,6 +74,15 @@ void app_main(void)
     /* NimBLE stack initialization */
     ble_init();
     ble_set_callbacks(command_cb);
+    ms_event_group = xEventGroupCreate();
+    BaseType_t tret = xTaskCreate(ms_response_task,"co_sensor_periodic_task",5120,NULL,1,&ms_response_task_handle);
+    if (tret == pdPASS) {
+        ESP_LOGI(TAG,"Created ms_response_task");
+    }
+    else {
+        ESP_LOGE(TAG,"Creating ms_response_task not successful");
+    }
+
 again:
     ESP_LOGI(TAG, "Waiting for %d seconds",CONFIG_SILENT_TIME);
     vTaskDelay(pdMS_TO_TICKS(CONFIG_SILENT_TIME * 1000));
